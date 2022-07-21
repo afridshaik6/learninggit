51a3dc7d665a35d5896a0c353e03892c4a9e40f2
Afrid <afridshaik6@gmail.com>
2022-07-21 21:46:56 +0530

    added a file

--- duplicate.py
+++ duplicate.py
@@ -6,5 +6,7 @@ def sort_duplicates(input_str):
             output_list.append(i)
         else:
             elements_present.add(i)
-    return ''.join(sorted(output_list))
+    return ''.join(sorted(output_list)
+
+
 
